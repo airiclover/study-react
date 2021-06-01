d6fef8acf1445fac8415ad3751bbecd809a134c8
airi <airi-love-clover@rakuten.jp>
2021-06-01 23:52:15 +0900

    ✨

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(
     (e) => {
@@ -18,11 +20,23 @@ export default function Home() {
     [count]
   );
 
+  const handleDisplay = () => {
+    setIsShow((isShow) => !isShow);
+  };
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてね");
+      return;
+    }
+    setText(e.target.value);
   }, []);
 
   return (
@@ -33,10 +47,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button href="/about" onClick={handleClick}>
         ボタン
       </button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
